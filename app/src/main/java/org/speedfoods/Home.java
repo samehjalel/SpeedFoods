d9a3287c4823b5b196e8356f4be04eb32370185b
samehjalel <jalloul03@outlook.fr>
2017-12-13 04:41:41 +0100

    FoodList of Menu work well

--- app/src/main/java/org/speedfoods/Home.java
+++ app/src/main/java/org/speedfoods/Home.java
@@ -1,5 +1,6 @@
 package org.speedfoods;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,6 +38,7 @@ public class Home extends AppCompatActivity
     TextView txtFullName;
     RecyclerView recyler_menu;
     RecyclerView.LayoutManager layoutManager;
+    FirebaseRecyclerAdapter <Category,MenuViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,7 +91,7 @@ public class Home extends AppCompatActivity
 
     private void loadMenu() {
 
-        FirebaseRecyclerAdapter <Category,MenuViewHolder> adapter=new FirebaseRecyclerAdapter <Category,MenuViewHolder>(Category.class,R.layout.menu_item,MenuViewHolder.class,category){
+        adapter=new FirebaseRecyclerAdapter <Category,MenuViewHolder>(Category.class,R.layout.menu_item,MenuViewHolder.class,category){
 
             @Override
             protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
@@ -100,7 +102,14 @@ public class Home extends AppCompatActivity
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(Home.this,""+clickItem.getName(),Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(Home.this,""+clickItem.getName(),Toast.LENGTH_SHORT).show();
+
+                        //get categoryId and send it new activity
+
+                        Intent foodList=new Intent(Home.this,FoodList.class);
+                        //because categoryId is key, so we just get key of item
+                        foodList.putExtra("CategoryId",adapter.getRef(position).getKey());
+                        startActivity(foodList);
                     }
                 });
 
